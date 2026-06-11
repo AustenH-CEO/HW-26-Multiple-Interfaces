6c7336885e4f78bae9b2f68157b9faf6cacc803f
austen.hernandez <austenh05@gmail.com>
2026-06-11 14:12:20 -0600

    created source code

--- Program.cs
+++ Program.cs
@@ -7,7 +7,9 @@ public class Program
     public static void Main()
     {
         Drone drone = new Drone();
-
+        drone.FlyUp();
+        drone.Shoot();
+        drone.FlyDown();
     }
 }
 public interface IFlyable
@@ -24,13 +26,16 @@ public class Drone : IFlyable, IShootable
     public void FlyUp()
     {
         Console.WriteLine("Drone flies upward.");
+        Console.WriteLine("");
     }
     public void FlyDown()
     {
         Console.WriteLine("Drone flies downward.");
+        Console.WriteLine("");
     }
     public void Shoot()
     {
         Console.WriteLine("Drone fires lasers.");
+        Console.WriteLine("");
     }
 }
