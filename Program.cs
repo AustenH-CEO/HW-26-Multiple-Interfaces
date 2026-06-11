cdb9c0cbebea1e86f678f33c0e7f8ed2831b15f9
austen.hernandez <austenh05@gmail.com>
2026-06-11 14:09:56 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// This program will demonstrate implementing multiple interfaces in a single class.
+// Austen Hernandez
+// 6/11/26
+
+public class Program
+{
+    public static void Main()
+    {
+        Drone drone = new Drone();
+
+    }
+}
+public interface IFlyable
+{
+    void FlyUp();
+    void FlyDown();
+}
+public interface IShootable
+{
+    void Shoot();
+}
+public class Drone : IFlyable, IShootable
+{
+    public void FlyUp()
+    {
+        Console.WriteLine("Drone flies upward.");
+    }
+    public void FlyDown()
+    {
+        Console.WriteLine("Drone flies downward.");
+    }
+    public void Shoot()
+    {
+        Console.WriteLine("Drone fires lasers.");
+    }
+}
